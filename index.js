f00ef474355ca0601fbd91930982c393e21fd21b
phenyopilatso <pilatsophenyo@gmail.com>
2024-07-10 23:30:12 +0200

    create api

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const todoRoutes = require('./routes/todoRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/todos', todoRoutes);
+
+mongoose.connect('mongodb://localhost:27017/todoapp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ models/todo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
